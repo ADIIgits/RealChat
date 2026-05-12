b865cd391712591c5a9f1463a6fec640507b408a
ADIIgits <adityalpu27@gmail.com>
2026-05-12 22:02:51 +0530

    bringing back original web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
-
+use App\Http\Controllers\ChannelController;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return auth()->check() ? redirect()->route('channels.index') : redirect()->route('login');
 });
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/channels', [ChannelController::class, 'index'])->name('channels.index');
+    Route::post('/channels', [ChannelController::class, 'store'])->name('channels.store');
+    Route::get('/channels/{channel}', [ChannelController::class, 'show'])->name('channels.show');
+    Route::post('/channels/{channel}/join', [ChannelController::class, 'join'])->name('channels.join');
+
+    Route::post('/channels/{channel}/messages', [MessageController::class, 'store'])->name('messages.store');
+    Route::post('/channels/{channel}/typing', [MessageController::class, 'typing'])->name('messages.typing');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
